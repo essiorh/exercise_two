4c2f5b5021d1c5c9577f47ebe93367dd7d22c4b5
Ilia Maltsev <maltsev.ilia@outlook.com>
2015-05-31 14:52:26 +0700

    Mission completed

--- app/src/main/java/com/example/iliamaltsev/exercise_two/Operation.java
+++ app/src/main/java/com/example/iliamaltsev/exercise_two/Operation.java
@@ -6,28 +6,28 @@ import java.io.Serializable;
  * Created by Ilia Maltsev on 30.05.2015.
  */
 public class Operation implements Serializable{
-    private int firstFragment;
-    private int secondFiagment;
-    public Operation(int firstFragment,int secondFiagment)
+    private String  firstFragment;
+    private String secondFiagment;
+    public Operation(String firstFragment,String secondFiagment)
     {
         this.firstFragment=firstFragment;
         this.secondFiagment=secondFiagment;
     }
     public Operation(){}
 
-    public int getFirstFragment() {
+    public String getFirstFragment() {
         return firstFragment;
     }
 
-    public void setFirstFragment(int firstFragment) {
+    public void setFirstFragment(String firstFragment) {
         this.firstFragment = firstFragment;
     }
 
-    public int getSecondFiagment() {
+    public String getSecondFiagment() {
         return secondFiagment;
     }
 
-    public void setSecondFiagment(int secondFiagment) {
+    public void setSecondFiagment(String secondFiagment) {
         this.secondFiagment = secondFiagment;
     }
 }
--- app/src/main/java/com/example/iliamaltsev/exercise_two/SecondActivity.java
+++ app/src/main/java/com/example/iliamaltsev/exercise_two/SecondActivity.java
@@ -3,6 +3,7 @@ package com.example.iliamaltsev.exercise_two;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -11,7 +12,6 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.FrameLayout;
 import android.widget.Spinner;
 
 import java.util.ArrayList;
@@ -19,38 +19,31 @@ import java.util.Collections;
 
 
 public class SecondActivity extends AppCompatActivity implements View.OnClickListener,AdapterView.OnItemSelectedListener,
-                                                                    ImageFragment.OnSelectedButtonListener{
-    private ArrayList<Operation> listOpetation=new ArrayList<>();
-    FrameLayout frameLayoutImage;
-    ImageFragment fragmentOne;
-    ImageFragment fragmentTwo;
-    ImageFragment fragmentThree;
-    ImageFragment fragmentFour;
-    ImageFragment fragmentFive;
-    ImageFragment fragmentSix;
-    ImageFragment fragmentFirst;
+                                                                    ImageFragment.OnSelectedButtonListener {
+    private ArrayList<Operation> listOperation = new ArrayList<>();
+    private ImageFragment fragmentFirst;
+    private Drawable drawableFirst;
+
     @Override
-    public void onButtonSelected(int buttonIndex) {
-
+    public void onButtonSelected(int buttonIndex, Drawable drawableIndex) {
         if (fragmentFirst != null) {
-            String tagThis = fragmentFirst.getTag();
             FragmentManager fragmentManager = getFragmentManager();
-
-            // Получаем ссылку на второй фрагмент по ID
-            ImageFragment fragmentNext= (ImageFragment) fragmentManager
+            ImageFragment fragmentNext = (ImageFragment) fragmentManager
                     .findFragmentByTag(String.valueOf(buttonIndex));
-            fragmentFirst.setDescription(Integer.valueOf(fragmentNext.getTag()));
-            fragmentNext.setDescription(Integer.valueOf(fragmentFirst.getTag()));
-
-            fragmentFirst = null;
+            if (!fragmentFirst.equals(fragmentNext)) {
+                fragmentFirst.setDescription(drawableIndex);
+                fragmentNext.setDescription(drawableFirst);
+                fragmentFirst.setBorder(false);
+                listOperation.add(new Operation(fragmentFirst.getTag(), fragmentNext.getTag()));
+                drawableFirst = null;
+                fragmentFirst = null;
+            }
         } else {
-            // подключаем FragmentManager
+            drawableFirst = drawableIndex;
             FragmentManager fragmentManager = getFragmentManager();
-
-            // Получаем ссылку на второй фрагмент по ID
-            fragmentFirst= (ImageFragment) fragmentManager
+            fragmentFirst = (ImageFragment) fragmentManager
                     .findFragmentByTag(String.valueOf(buttonIndex));
-
+            fragmentFirst.setBorder(true);
         }
     }
 
@@ -59,45 +52,34 @@ public class SecondActivity extends AppCompatActivity implements View.OnClickLis
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        Button buttonFinish=(Button)findViewById(R.id.button_finish);
+        Button buttonFinish = (Button) findViewById(R.id.button_finish);
+        buttonFinish.setText(getString(R.string.finish));
         buttonFinish.setOnClickListener(this);
-        Spinner ourSpinner=(Spinner)findViewById(R.id.our_spinner);
-        ArrayAdapter<CharSequence> arrayAdapter=ArrayAdapter.createFromResource(this, R.array.scale_names, android.R.layout.simple_spinner_item);
+        Spinner ourSpinner = (Spinner) findViewById(R.id.our_spinner);
+        ArrayAdapter<CharSequence> arrayAdapter = ArrayAdapter.createFromResource(this, R.array.scale_names, android.R.layout.simple_spinner_item);
         arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         ourSpinner.setAdapter(arrayAdapter);
         ourSpinner.setOnItemSelectedListener(this);
         FragmentManager fragmentManager = getFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentOne=ImageFragment.newInstance("1", "FIT_XY");
-        fragmentTwo=ImageFragment.newInstance("2", "FIT_XY");
-        fragmentThree=ImageFragment.newInstance("3", "FIT_XY");
-        fragmentFour=ImageFragment.newInstance("4", "FIT_XY");
-        fragmentFive=ImageFragment.newInstance("5", "FIT_XY");
-        fragmentSix=ImageFragment.newInstance("6", "FIT_XY");
-        fragmentTransaction.add(R.id.one, fragmentOne, "1");
-        fragmentTransaction.add(R.id.two, fragmentTwo, "2");
-        fragmentTransaction.add(R.id.three, fragmentThree, "3");
-        fragmentTransaction.add(R.id.four, fragmentFour, "4");
-        fragmentTransaction.add(R.id.five, fragmentFive, "5");
-        fragmentTransaction.add(R.id.six, fragmentSix, "6");
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction().setCustomAnimations(R.animator.animator, R.animator.animator_end);
+        ;
+        fragmentTransaction.add(R.id.one, ImageFragment.newInstance("1", "FIT_XY"), "1");
+        fragmentTransaction.add(R.id.two, ImageFragment.newInstance("2", "FIT_XY"), "2");
+        fragmentTransaction.add(R.id.three, ImageFragment.newInstance("3", "FIT_XY"), "3");
+        fragmentTransaction.add(R.id.four, ImageFragment.newInstance("4", "FIT_XY"), "4");
+        fragmentTransaction.add(R.id.five, ImageFragment.newInstance("5", "FIT_XY"), "5");
+        fragmentTransaction.add(R.id.six, ImageFragment.newInstance("6", "FIT_XY"), "6");
         fragmentTransaction.commit();
-
-        /*
-        findViewById(R.id.one).setOnClickListener(this);
-        findViewById(R.id.two).setOnClickListener(this);
-        findViewById(R.id.three).setOnClickListener(this);
-        findViewById(R.id.four).setOnClickListener(this);
-        findViewById(R.id.five).setOnClickListener(this);
-        findViewById(R.id.six).setOnClickListener(this);*/
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem menuItem) {
         switch (menuItem.getItemId()) {
             case android.R.id.home:
                 Intent homeIntent = new Intent(this, StartActivity.class);
                 homeIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                Bundle bundle=new Bundle();
-                bundle.putSerializable("mas",listOpetation);
+                Bundle bundle = new Bundle();
+                bundle.putSerializable("mas", listOperation);
                 homeIntent.putExtra("mas", bundle);
                 setResult(RESULT_OK, homeIntent);
                 finish();
@@ -109,8 +91,8 @@ public class SecondActivity extends AppCompatActivity implements View.OnClickLis
     public void onBackPressed() {
         Intent homeIntent = new Intent(this, StartActivity.class);
         homeIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        Bundle bundle=new Bundle();
-        bundle.putSerializable("mas", listOpetation);
+        Bundle bundle = new Bundle();
+        bundle.putSerializable("mas", listOperation);
         homeIntent.putExtra("mas", bundle);
         setResult(RESULT_OK, homeIntent);
         finish();
@@ -118,41 +100,23 @@ public class SecondActivity extends AppCompatActivity implements View.OnClickLis
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_second, menu);
         return true;
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.button_finish:
                 Intent homeIntent = new Intent(this, StartActivity.class);
                 homeIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                Bundle bundle=new Bundle();
-                bundle.putSerializable("mas", listOpetation);
+                Bundle bundle = new Bundle();
+                bundle.putSerializable("mas", listOperation);
                 homeIntent.putExtra("mas", bundle);
                 setResult(RESULT_OK, homeIntent);
                 finish();
                 break;
-            /*default:
-                if (frameLayoutImage==null){
-                    frameLayoutImage=(FrameLayout)findViewById(R.id.one);
-                }
-                else
-                replaceImages(v.getId());
-                findViewById(v.getId()).setBackgroundColor(getResources().getColor(R.color.primary));
-                break;
-                */
         }
-    }
-
-    private void replaceImages(int secondId) {
-        int firstId=frameLayoutImage.getId();
-        FragmentTransaction ft = getFragmentManager().beginTransaction().setCustomAnimations(R.animator.animator,R.animator.animator_end);
-        ft.replace(firstId, ImageFragment.newInstance("two", "FIT_XY"));
-        ft.replace(secondId, ImageFragment.newInstance("one", "FIT_XY"));
-        ft.commit();
     }
 
     @Override
@@ -161,22 +125,11 @@ public class SecondActivity extends AppCompatActivity implements View.OnClickLis
         Collections.addAll(arrayList, getResources().getStringArray(R.array.scale_values));
         String sScaleType = arrayList.get(position).toString();
         FragmentManager fragmentManager = getFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentOne=ImageFragment.newInstance("1", sScaleType);
-        fragmentTwo=ImageFragment.newInstance("2", sScaleType);
-        fragmentThree=ImageFragment.newInstance("3", sScaleType);
-        fragmentFour=ImageFragment.newInstance("4", sScaleType);
-        fragmentFive=ImageFragment.newInstance("5", sScaleType);
-        fragmentSix=ImageFragment.newInstance("6", sScaleType);
-        fragmentTransaction.add(R.id.one, fragmentOne, "1");
-        fragmentTransaction.add(R.id.two, fragmentTwo, "2");
-        fragmentTransaction.add(R.id.three, fragmentThree, "3");
-        fragmentTransaction.add(R.id.four, fragmentFour, "4");
-        fragmentTransaction.add(R.id.five, fragmentFive, "5");
-        fragmentTransaction.add(R.id.six, fragmentSix, "6");
-        fragmentTransaction.commit();
+        for (int i = 1; i <= 6; i++) {
+            ((ImageFragment) fragmentManager
+                    .findFragmentByTag(String.valueOf(i))).setScale(sScaleType);
+        }
     }
-
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
